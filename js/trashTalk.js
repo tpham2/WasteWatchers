1967f71329876c5421b8867c909fa758348cf007
Samm Iwamasa <siwamasa@ucsd.edu>
2018-11-06 16:59:01 -0800

    deleted commented code since it is unncessary

--- js/trashTalk.js
+++ js/trashTalk.js
@@ -22,25 +22,3 @@ $(document).ready(function(){
     }
   }
 })
-/*
-    // Activate Carousel
-    $("#myCarousel").carousel();
-
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".left").click(function(){
-            $("#myCarousel").carousel("prev");
-        });
-        $(".right").click(function(){
-            $("#myCarousel").carousel("next");
-        });
-    });
-*/
