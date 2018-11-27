4d2e775135cae86db125c5dc60cc78689bbad97d
Samm Iwamasa <siwamasa@ucsd.edu>
2018-11-26 22:06:34 -0800

    created search.js and linked it to all pages

---
+++ js/search.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  //search bar
+        $("#wrap").click(extendSearch);
+        var newTitle = document.getElementById('newTitle');
+        function extendSearch() {
+          if (newTitle.style.display === "none") {
+            newTitle.style.display = "block";
+          } 
+          else {
+            newTitle.style.display = "none";
+          }
+        }
+
+  // Search function
+
+     var searchInput = document.getElementById("search");
+
+      searchInput.addEventListener("keydown", function (e) {
+          if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+          console.log('hello');
+    $("#search_submit").click();
+          }
+      });
+
+      $('#search_submit').click(searchCheck);
+
+        function searchCheck() {
+          var itemFound = false;
+
+           switch (searchInput.value) {
+             case ("apple"):
+               console.log(searchInput.value);
+               itemFound = true;
+               open("appleSearchResult.html", "_self")
+               break;
+             case ("banana"):
+               console.log(searchInput.value);
+               itemFound = true;
+               open("bananaSearchResult.html", "_self")
+               break;
+             default:
+               console.log('No Items Found');
+           }
+           searchInput.value = '';
+         }
+
+});
